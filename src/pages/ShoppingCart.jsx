8bb3616fcfda107d07b4ca11653485d32bf5b8ba
Paulo Alexandre <machitachialexandre@gmail.com>
2022-04-17 21:18:31 -0300

    Carrinho de compra - em andamento

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class ShoppingCart extends Component {
   render() {
     return (
-      <div>ShoppingCart</div>
+      <>
+        <Header />
+        <div>ShoppingCart</div>
+      </>
     )
   }
 }
